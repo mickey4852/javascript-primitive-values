b928f64c1a9871debf7d0f665188d1e294769907
Kuntika Siengsukhon <atgenda@gmail.com>
2025-04-16 13:08:25 +0700

    feat: add all exercise answers

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalProductPrice = 27000;
+let discountTotalPrice = totalProductPrice*0.25;
+let netPrice = totalProductPrice-discountTotalPrice;
+
+console.log(netPrice);
